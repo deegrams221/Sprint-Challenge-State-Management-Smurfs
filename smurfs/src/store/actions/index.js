cf959b7105808856d8a608079aac3fca0954529c
Diana Grams <deegrams221@gmail.com>
2019-09-13 10:19:51 -0600

    set up action creators/types

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -2,3 +2,43 @@
 //  import Axios and set up dispatch
 //    axios.get(`http://localhost:3333/smurfs`)
 //    axios.post('http://localhost:3333/smurfs'): name: '', age: [], height: '', id: []
+
+import axios from 'axios';
+
+// action types
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+export const SEND_SMURF_START = 'SEND_SMURF_START';
+export const SEND_SMURF_SUCCESS = 'SEND_SMURF_SUCCESS';
+
+// action creators
+export const sendData = () => dispatch =>  {
+  // dispatch and axios request - send
+  dispatch({type: SEND_SMURF_START});
+    axios
+      .post('http://localhost:3333/smurfs', {
+            name: '',
+            age: [],
+            height: '',
+            id: []
+      })
+      .then(res => {
+        dispatch({type: SEND_SMURF_SUCCESS, payload: res.data});
+      });
+};
+
+export const getData = () => dispatch => {
+  // dispatch and axios request - fetch
+    dispatch({ type: FETCH_SMURF_START });
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        // console.log("axios request: ", res.data)
+        dispatch({type: FETCH_SMURF_SUCCESS, payload: res.data});
+      })
+      .catch(error => {
+        // console.log("catch error: ", error.res.message);
+        dispatch({type: FETCH_SMURF_FAILURE, payload: error.response});
+      });
+}; 
