7a71c7691f3b8544d89a8df437b29ca55fe19295
Diana Grams <deegrams221@gmail.com>
2019-09-13 10:30:28 -0600

    set up reducers

--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,6 +1,8 @@
 // Set up initialState & reducers
 // set up Reducer logic for actions
 
+import {FETCH_SMURF_START, FETCH_SMURF_SUCCESS} from '../actions';
+
 const initialState = {
   smurfs: [
     {
@@ -16,7 +18,19 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
-
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      };
+      case FETCH_SMURF_SUCCESS:
+        return {
+          ...state,
+          isLoading: false,
+          smurfs: action.payload,
+          error: ''
+        };
     default:
       return state;
   }
